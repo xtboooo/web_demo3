c6a28fc6627535eaee7deef3805e222799bfe68c
xtbo97 <xtbo9719@gmail.com>
2020-12-09 19:31:11 +0800

    注册页面重定向到登陆页面

--- web_demo3/users/views.py
+++ web_demo3/users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from users.models import User
 
 
 def register(request):
@@ -7,5 +8,12 @@ def register(request):
     else:
         username = request.POST.get('username')
         password = request.POST.get('password')
+        user = User.objects.create(username=username, password=password)
         print(f'username:{username} password:{password}')
-        return HttpResponse('注册成功')
+        return redirect('/login/')
+
+
+def login(request):
+    """登陆 View 视图函数"""
+    return render(request, 'login.html')
+
